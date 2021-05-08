33796b18365ba759a1d5faca6b40d14becd97551
Kamil Józefowski <k.m.jozefowski@gmail.com>
2021-05-08 19:00:24 +0200

    Add netflix-hystrix, Circuit Breaker and sleuth

--- src/main/java/com/kodilla/customer/connector/AccountsConnector.java
+++ src/main/java/com/kodilla/customer/connector/AccountsConnector.java
@@ -1,12 +1,26 @@
 package com.kodilla.customer.connector;
 
 import com.kodilla.customer.domian.GetAccountsResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "accounts")
+import java.util.Collections;
+
+@FeignClient(name = "accounts", fallback = AccountsConnectorFallback.class)
 public interface AccountsConnector {
     @GetMapping("/v1/accounts")
     GetAccountsResponse getAccounts(@RequestParam("customerId") Long customerId);
+}
+
+@Slf4j
+@Component
+class AccountsConnectorFallback implements AccountsConnector {
+    @Override
+    public GetAccountsResponse getAccounts(Long customerId) {
+        log.warn("Can not get account for customerId: {}", customerId);
+        return new GetAccountsResponse(Collections.emptyList());
+    }
 }
